b99f119ac2b0e9da0be8c816a87d4f411ff32265
justdanpo <justdanpo@gmail.com>
2016-10-02 00:08:21 +0300

    newtab-middleclick-pasteandgo.js: process tabstrip recreation

--- vivaldi/hooks/newtab-middleclick-pasteandgo.js
+++ vivaldi/hooks/newtab-middleclick-pasteandgo.js
@@ -1,35 +1,54 @@
-//MiddleClick on New Tab button to Paste And Go
-//Клик средней кнопкой мыши по кнопке "[+]" открывает ссылку из буфера
-
-vivaldi.jdhooks.onUIReady(function() {
-
-    var newtabbutton = document.querySelector('div.tab-strip > button.button-tabbar');
-
-    newtabbutton.addEventListener('click', function(e) {
-
-        if (e.button === 1) {
-
-            var txt;
-            var getClipboard = function(t) {
-                if (t.clipboardData.types.indexOf("text/plain") > -1) {
-                    txt = t.clipboardData.getData("text/plain");
-                    t.preventDefault();
-                }
-            };
-            document.addEventListener("paste", getClipboard);
-            document.execCommand("paste");
-            document.removeEventListener("paste", getClipboard);
-
-            vivaldi.jdhooks.require('_UrlFieldActions').go(txt, {
-                inCurrent: !1,
-                addTypedHistory: !0,
-                addTypedSearchHistory: !1,
-                enableSearch: !0
-            });
-
-            e.stopPropagation();
-            e.preventDefault();
-        }
-    });
-
+//MiddleClick on New Tab button to Paste And Go
+//Клик средней кнопкой мыши по кнопке "[+]" открывает ссылку из буфера
+
+vivaldi.jdhooks.onUIReady(function() {
+
+    var setupButton = function(item) {
+        var newtabbutton = item.querySelector('div.tab-strip > button.button-tabbar');
+
+        if (!newtabbutton) return;
+
+        newtabbutton.addEventListener('click', function(e) {
+
+            if (e.button === 1) {
+
+                var txt;
+                var getClipboard = function(t) {
+                    if (t.clipboardData.types.indexOf("text/plain") > -1) {
+                        txt = t.clipboardData.getData("text/plain");
+                        t.preventDefault();
+                    }
+                };
+                document.addEventListener("paste", getClipboard);
+                document.execCommand("paste");
+                document.removeEventListener("paste", getClipboard);
+
+                vivaldi.jdhooks.require('_UrlFieldActions').go(txt, {
+                    inCurrent: !1,
+                    addTypedHistory: !0,
+                    addTypedSearchHistory: !1,
+                    enableSearch: !0
+                });
+
+                e.stopPropagation();
+                e.preventDefault();
+            }
+        });
+    };
+    setupButton(document);
+
+
+    var obs = new MutationObserver(function(mutations, observer) {
+        mutations.forEach(function(mutation) {
+            mutation.addedNodes.forEach(function(itm) {
+                setupButton(itm);
+            });
+        });
+    });
+
+    obs.observe(document.querySelector('#header'), {
+        childList: true,
+        subtree: true
+    });
+
 });
