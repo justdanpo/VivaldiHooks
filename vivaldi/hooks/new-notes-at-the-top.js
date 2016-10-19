ee266a319a03bc64aff7b42a8fe8d041bce152b0
den_po <jdp@bk.ru>
2016-10-19 20:13:22 +0300

    new-notes-at-the-top.js: fixed adding from a context menu

--- vivaldi/hooks/new-notes-at-the-top.js
+++ vivaldi/hooks/new-notes-at-the-top.js
@@ -5,12 +5,24 @@ vivaldi.jdhooks.hookMember(window.vivaldi.notes, 'create', function(hookData, no
 
     var notesObject = this;
 
-    vivaldi.jdhooks.hookMember(hookData.arguments, '1', function(hoodata, item) {
-
+    function moveToTop(item) {
         notesObject.move(item.id, {
             index: 0
         });
+    }
 
-    });
-
+    switch (hookData.arguments.length) {
+        case 0:
+            //we can't do anything
+            break;
+        case 1:
+            hookData.arguments[1] = moveToTop;
+            hookData.arguments.length = 2;
+            break;
+        case 2:
+            vivaldi.jdhooks.hookMember(hookData.arguments, '1', function(hoodata, item) {
+                moveToTop(item);
+            });
+            break;
+    }
 });
