65994e0cb750201c7611c1642dfeb791d6a23655
den_po <jdp@bk.ru>
2016-12-01 11:22:50 +0300

    new-notes-at-the-top.js: much simplier

--- vivaldi/hooks/new-notes-at-the-top.js
+++ vivaldi/hooks/new-notes-at-the-top.js
@@ -3,26 +3,7 @@
 
 vivaldi.jdhooks.hookMember(window.vivaldi.notes, 'create', function(hookData, note, callback) {
 
-    var notesObject = this;
+    if (undefined === note.index)
+        note.index = 0;
 
-    function moveToTop(item) {
-        notesObject.move(item.id, {
-            index: 0
-        });
-    }
-
-    switch (hookData.arguments.length) {
-        case 0:
-            //we can't do anything
-            break;
-        case 1:
-            hookData.arguments[1] = moveToTop;
-            hookData.arguments.length = 2;
-            break;
-        case 2:
-            vivaldi.jdhooks.hookMember(hookData.arguments, '1', function(hoodata, item) {
-                moveToTop(item);
-            });
-            break;
-    }
 });
