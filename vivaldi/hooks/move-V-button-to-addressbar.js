3488a45696aae18c92f88b43c7bd83c0ed393a88
den_po <jdp@bk.ru>
2017-01-06 01:24:16 +0300

    mailbar compatibility

--- vivaldi/hooks/move-V-button-to-addressbar.js
+++ vivaldi/hooks/move-V-button-to-addressbar.js
@@ -2,97 +2,68 @@
 
 (function() {
 
-    var newButton = null;
+    var style = document.createElement('style');
+    style.setAttribute('description', 'added by move-V-button-to-addressbar.js');
+    style.textContent =
+        "#browser #tabs-container.top{ padding-left: 0px }" +
+        "#browser.native:not(.tabs-top):not(.horizontal-menu) #header { display: none } " +
+        "#titlebar > .vivaldi { display: none }" +
+        "#browser.horizontal-menu .vivaldi-addressbar {display: none }";
+    document.head.appendChild(style);
+
+    function showMenu(event) {
+        var button = document.querySelector('#browser:not(.horizontal-menu) .vivaldi-addressbar');
+        if (button) {
+            vivaldi.jdhooks.require('_ShowMenu')(
+                vivaldi.jdhooks.require('_CommandManager').getVerticalMenu(
+                    vivaldi.jdhooks.require('_PageStore').getPages()
+                ),
+                null, "bottom", button
+            )(event);
+            return true;
+        }
+        return false;
+    }
 
     //alt key menu
     vivaldi.jdhooks.hookClass('TitleBar', function(reactClass) {
-
         vivaldi.jdhooks.hookMember(reactClass, 'showMenu', function(hookData, event) {
-
-            if (newButton) {
+            if (showMenu(event))
                 hookData.abort();
-                return vivaldi.jdhooks.require('_ShowMenu')(
-                    vivaldi.jdhooks.require('_CommandManager').getVerticalMenu(this.state.pages),
-                    null,
-                    "bottom",
-                    newButton,
-                    null,
-                    null,
-                    this.activeShortcuts
-                )(event);
-            }
-
-        }, null);
+        });
     });
 
-    vivaldi.jdhooks.onUIReady(function() {
 
-        var titlebar = document.querySelector('#titlebar');
-        var toolbar = document.querySelector('#main > .toolbar');
+    function hookRender(reactClass) {
+        vivaldi.jdhooks.hookMember(reactClass, 'render', null, function(hookData) {
 
-        var moveButton = function(itm) {
-
-            newButton = itm.cloneNode(true);
-            newButton.style.top = 0;
-            newButton.style.left = 0;
-            newButton.style.position = 'relative';
-            newButton.style.height = 'auto';
-            newButton.className = 'vivaldi-addressbar button-toolbar';
-            newButton.onmousedown = function(event) {
-
-                vivaldi.jdhooks.require('_ShowMenu')(
-                    vivaldi.jdhooks.require('_CommandManager').getVerticalMenu(
-                        vivaldi.jdhooks.require('_PageStore').getPages()
-                    ),
-                    null, "bottom", newButton
-                )(event);
-
-            };
-
-            var vivaldiv = newButton.querySelector('span.vivaldi-v');
-            if (vivaldiv)
-                vivaldiv.parentNode.removeChild(vivaldiv);
-
-            toolbar.appendChild(newButton);
-            itm.style.left = '-100px';
-
-            var style = document.createElement('style');
-            style.setAttribute('description', 'added by move-V-button-to-extensions.js');
-            style.textContent =
-                "#browser #tabs-container.top{ padding-left: 0px }" +
-                "#browser.native:not(.tabs-top) #header { display: none }";
-            document.head.appendChild(style);
-        };
-
-        var vButton = titlebar.querySelector('button.vivaldi');
-        if (vButton) moveButton(vButton);
-
-        var obs = new MutationObserver(function(mutations, observer) {
-
-            mutations.forEach(function(mutation) {
-
-                mutation.addedNodes.forEach(function(itm) {
-                    if (itm.className === "vivaldi") {
-                        moveButton(itm);
+            hookData.retValue.props.children.push(
+                vivaldi.jdhooks.require('react_React').createElement('div', {
+                        className: 'vivaldi-addressbar button-toolbar',
+                        ref: 'movedVButton',
+                        dangerouslySetInnerHTML: {
+                            __html: vivaldi.jdhooks.require(
+                                '_svg_vivaldi_title'
+                                //'_svg_menu_vivaldi'
+                                //'_svg_vivaldi_v'
+                            )
+                        },
+                        onClick: showMenu.bind(this)
                     }
-                });
 
-                mutation.removedNodes.forEach(function(itm) {
-                    if (itm.className === "vivaldi") {
-                        if (newButton) {
-                            newButton.parentNode.removeChild(newButton);
-                            newButton = null;
-                        }
-                    }
-                });
+                )
+            );
 
-            });
+            return hookData.retValue;
         });
-        obs.observe(titlebar, {
-            childList: true,
-            subtree: false
-        });
+    }
 
+    vivaldi.jdhooks.hookClass('UrlBar', function(reactClass) {
+        hookRender(reactClass)
+    });
+
+    vivaldi.jdhooks.hookClass('MailBar', function(reactClass) {
+        hookRender(reactClass)
     });
 
 })();
