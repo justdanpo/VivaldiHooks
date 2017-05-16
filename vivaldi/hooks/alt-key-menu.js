058f1f0ee519a87e89c8ec19037a54493b657d4d
Your Name <you@example.com>
2017-05-17 00:08:46 +0300

    alt-key-menu.js & jdhooks-startup-settings.js: fix for 1.10 todo: download-tab.js, qc-close-tab.js, searchfield-current-engine-icon.js, speeddial-items-geometry.js(*)

--- vivaldi/hooks/alt-key-menu.js
+++ vivaldi/hooks/alt-key-menu.js
@@ -1,11 +1,22 @@
-//Prevent Shift+Alt menu appearance
-//Не открывать меню по Shift+Alt
-
-vivaldi.jdhooks.hookClass('TitleBar', function(reactClass) {
-    vivaldi.jdhooks.hookMember(reactClass, 'onKeyDown', function(hookData, event) {
-        if (event.key === "Alt" && (event.shiftKey || event.ctrlKey)) {
-            hookData.abort();
-            this.stopMenuDetection();
-        }
-    });
+//Prevent Shift+Alt menu appearance
+//Не открывать меню по Shift+Alt
+
+vivaldi.jdhooks.hookModule('VivaldiSettingsWrapper', function(moduleInfo) {
+    vivaldi.jdhooks.hookMember(moduleInfo, 'exports', function(hookData, fn, settingsKeys) {
+
+        //TitleBar
+        if ((settingsKeys.indexOf("VIVALDI_MENU_POSITION") > -1) && (-1 !== (fn.prototype.render + '').indexOf('"titlebar"'))) {
+
+            vivaldi.jdhooks.hookMember(fn.prototype, 'componentWillMount', function(hookData) {
+
+                var _this = this;
+                vivaldi.jdhooks.hookMember(this, 'onKeyDown', function(hookData, event) {
+                    if (event.key === "Alt" && (event.shiftKey || event.ctrlKey)) {
+                        hookData.abort();
+                        _this.stopMenuDetection();
+                    }
+                });
+            });
+        }
+    })
 });
