6a709b71248587dab7f98fb8516068fdd2e27dca
den_po <jdp@bk.ru>
2020-04-12 21:04:28 +0300

    ctrl-middleclick-reset-zoom.js

---
+++ vivaldi/hooks/ctrl-middleclick-reset-zoom.js
@@ -0,0 +1,19 @@
+//Ctrl+MiddleClick on a webpage contents - reset zoom
+//Ctrl+MiddleClick на содержимом страницы - сброс масштаба
+
+vivaldi.jdhooks.onUIReady(function () {
+    let keyCodes = vivaldi.jdhooks.require("_KeyCodes")
+
+    var node = document.querySelector(".inner")
+    if (node) {
+        let ctrl = false
+        window.vivaldi.tabsPrivate.onKeyboardChanged.addListener((pressed, keymask, keycode, some) => { if (keycode === keyCodes.KEY_CONTROL) ctrl = pressed })
+
+        node.addEventListener("mouseup", event => {
+            if (ctrl && event.button === 1 && event.srcElement.nodeName === "WEBVIEW") {
+                vivaldi.jdhooks.require("_PageZoom").pageZoomReset()
+                event.preventDefault()
+            }
+        })
+    }
+})
