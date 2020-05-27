53371294198ef0dcee23f1e28360fe43a1c4775a
Denis Porfiryev <denis.porfiryev@nokia.com>
2020-05-28 02:58:21 +0300

    speeddial-spacing.js cleanups

--- vivaldi/hooks/speeddial-spacing.js
+++ vivaldi/hooks/speeddial-spacing.js
@@ -1,51 +1,65 @@
 // Change SD tiles geometry (spacing etc.)
 
+vivaldi.jdhooks.hookModule("vivaldiSettings", (moduleInfo, exports) => {
+    let oldGetDefault = exports.getDefault
+    exports.getDefault = name => {
+        switch (name) {
+            case "SPEEDDIAL_SIZING": return {
+                widthHeightRatio: 1.2,
+                spacing: 5,
+                edgeMargin: 15
+            }
+            default: return oldGetDefault(name)
+        }
+    }
+    return exports
+})
+
 vivaldi.jdhooks.hookClass('speeddial_SpeedDialView', cls => {
-    // Could be avoided by using just strings instead of calling this
-    const PrefKeys = vivaldi.jdhooks.require('_PrefKeys');
-    const settings = vivaldi.jdhooks.require('vivaldiSettings');
-    class newSDView extends cls {
+    const PrefKeys = vivaldi.jdhooks.require('_PrefKeys')
+
+    return vivaldi.jdhooks.insertWatcher(class extends cls {
         constructor(...e) {
-            super(...e);
+            super(...e)
 
             // To get the original code, search for getSpeedDialGeometry, it’s
             // just a few matches
             this.getSpeedDialGeometry = () => {
-                let sets = {
-                    ...{
-                        widthHeightRatio: 1.2,
-                        spacing: 5,
-                        edgeMargin: 15
-                    },
-                    ...settings.getSync(['SPEEDDIAL_SIZING'])
-                };
-                let width = this.props.prefValues[PrefKeys.kStartpageSpeedDialWidth];
+                const sets = this.state.jdVivaldiSettings.SPEEDDIAL_SIZING
+
+                let width = this.props.prefValues[PrefKeys.kStartpageSpeedDialWidth]
                 // Adjust size automatically
                 if (width === -1) {
-                    const wth = Math.round(this.props.maxWidth / (1.0 * this.props.prefValues[PrefKeys.kStartpageSpeedDialColumns]));
-                    width = Math.max(Math.min(wth, 400), 100);
+                    const wth = Math.round(this.props.maxWidth / (1.0 * this.props.prefValues[PrefKeys.kStartpageSpeedDialColumns]))
+                    width = Math.max(Math.min(wth, 400), 100)
                 }
-                const height = Math.round(width / sets['widthHeightRatio']),
-                      spacing = sets['spacing'];
-                let count = this.state.dialNodes.length;
+                const height = Math.round(width / sets.widthHeightRatio)
+                const spacing = sets.spacing
+
+                let count = this.state.dialNodes.length
                 // One more for the ‘plus’ tile
-                this.props.prefValues[PrefKeys.kStartpageSpeedDialAddButtonVisible] && count++;
-                const spacedWidth = width + 2 * spacing;
-                let spacedHeight = height + 2 * spacing;
+                if (this.props.prefValues[PrefKeys.kStartpageSpeedDialAddButtonVisible]) count++
+
+                const spacedWidth = width + 2 * spacing
+
+                let spacedHeight = height + 2 * spacing
                 // Add space for titles
                 if (this.props.prefValues[PrefKeys.kStartpageSpeedDialTitlesVisible] !== "never")
-                    spacedHeight += 30;
-                const margin = sets['edgeMargin'],
-                      maxCols = this.props.prefValues[PrefKeys.kStartpageSpeedDialColumns] || 1000;
+                    spacedHeight += 30
+
+                const margin = sets.edgeMargin
+                const maxCols = this.props.prefValues[PrefKeys.kStartpageSpeedDialColumns] || 1000
+
                 let cols = Math.max(
-                               1,
-                               Math.min(
-                                   Math.floor(
-                                       (this.props.maxWidth - margin)
-                                       / spacedWidth),
-                                   maxCols));
+                    1,
+                    Math.min(
+                        Math.floor(
+                            (this.props.maxWidth - margin)
+                            / spacedWidth),
+                        maxCols))
                 if (this.props.maxWidth < (spacedWidth * cols + margin))
-                    cols = 1;
+                    cols = 1
+
                 return {
                     count: count,
                     cols: cols,
@@ -55,82 +69,58 @@ vivaldi.jdhooks.hookClass('speeddial_SpeedDialView', cls => {
                     thumbnailWidth: width,
                     thumbnailHeight: height,
                     dialSpace: spacing
-                };
-            };
+                }
+            }
         }
-    }
-    return newSDView;
-});
+    }, { settings: ["SPEEDDIAL_SIZING"] })
+})
 
 // Settings
 vivaldi.jdhooks.hookClass('settings_startpage_StartPage', cls => {
-    const React = vivaldi.jdhooks.require('React');
-    const settings = vivaldi.jdhooks.require('vivaldiSettings');
-    const settSrchCatChild = vivaldi.jdhooks.require('settings_SettingsSearchCategoryChild');
-    const PrefKeys = vivaldi.jdhooks.require('_PrefKeys');
-    const PrefSet = vivaldi.jdhooks.require('_PrefSet');
+    const React = vivaldi.jdhooks.require('React')
+    const settings = vivaldi.jdhooks.require('vivaldiSettings')
+    const settSrchCatChild = vivaldi.jdhooks.require('settings_SettingsSearchCategoryChild')
+    const PrefKeys = vivaldi.jdhooks.require('_PrefKeys')
+    const PrefSet = vivaldi.jdhooks.require('_PrefSet')
 
-    function setProperty(obj, name, value) {
-        if (name in obj) Object.defineProperty(obj, name, { value: value, enumerable: true, configurable: true, writable: true }); else obj[name] = value
-    }
-    class TilesSection extends React.Component {
-        constructor(...e) {
-            super(...e);
-
-            setProperty(this, 'state', {
-                ...{
-                    widthHeightRatio: 1.2,
-                    spacing: 5,
-                    edgeMargin: 15
-                },
-                ...settings.getSync(['SPEEDDIAL_SIZING'])
-            });
-        }
-
-        updateCustomSettings() {
-            settings.set({['SPEEDDIAL_SIZING']: {
-                widthHeightRatio: this.state.widthHeightRatio,
-                spacing: this.state.spacing,
-                edgeMargin: this.state.edgeMargin
-            }});
-        }
-
-        onInputChanged(setting, inputProperty, parseFunction, event) {
-            if (event.target && event.target[inputProperty] !== undefined) {
-                let newState = {};
-                newState[setting] = parseFunction.call(this, [event.target[inputProperty]]);
-                this.setState(newState, () => (this.updateCustomSettings()));
+    const TilesSection = vivaldi.jdhooks.insertWatcher(class extends React.Component {
+        onInputChanged(setting, parseFunction, event) {
+            if (event.target && event.target.value) {
+                let newState = { ... this.state.jdVivaldiSettings.SPEEDDIAL_SIZING, ...{ [setting]: parseFunction([event.target.value]) } }
+                settings.set({ ['SPEEDDIAL_SIZING']: newState })
             }
         }
 
         onWidthInputChanged(event) {
-            if (event.target && event.target.value !== undefined) {
-                let val = parseInt(event.target.value);
-                if (val < 100) val = -1;
-                PrefSet.set(PrefKeys.kStartpageSpeedDialWidth, val);
+            if (event.target && event.target.value) {
+                let val = parseInt(event.target.value)
+                if (val < 100) val = -1
+                PrefSet.set(PrefKeys.kStartpageSpeedDialWidth, val)
             }
         }
 
         onMaxColsInputChanged(event) {
-            if (event.target && event.target.value !== undefined) {
-                let val = parseInt(event.target.value);
-                if (val < 1) val = 1000;
-                PrefSet.set(PrefKeys.kStartpageSpeedDialColumns, val);
+            if (event.target && event.target.value) {
+                let val = parseInt(event.target.value)
+                if (val < 1) val = 1000
+                PrefSet.set(PrefKeys.kStartpageSpeedDialColumns, val)
             }
         }
 
         render() {
-            let tileWidth = this.props.prefValues[PrefKeys.kStartpageSpeedDialWidth];
-            let tileDisplayWidth = tileWidth.toString() + 'px';
+            const sets = this.state.jdVivaldiSettings.SPEEDDIAL_SIZING
+
+            let tileWidth = this.props.prefValues[PrefKeys.kStartpageSpeedDialWidth]
+            let tileDisplayWidth = tileWidth + 'px'
             if (tileWidth < 100) {
-                tileWidth = 90;
-                tileDisplayWidth = 'Auto';
+                tileWidth = 90
+                tileDisplayWidth = 'Auto'
             }
-            let maxCols = this.props.prefValues[PrefKeys.kStartpageSpeedDialColumns];
-            let maxColsDisplay = maxCols.toString();
+            let maxCols = this.props.prefValues[PrefKeys.kStartpageSpeedDialColumns]
+            let maxColsDisplay = maxCols
             if (maxCols > 20) {
-                maxCols = 0;
-                maxColsDisplay = 'Auto';
+                maxCols = 0
+                maxColsDisplay = 'Auto'
             }
             return React.createElement(settSrchCatChild,
                 { filter: this.props.filter },
@@ -163,10 +153,10 @@ vivaldi.jdhooks.hookClass('settings_startpage_StartPage', cls => {
                         min: 0.5,
                         max: 2,
                         step: 0.1,
-                        value: this.state.widthHeightRatio,
-                        onChange: this.onInputChanged.bind(this, 'widthHeightRatio', 'value', parseFloat)
+                        value: sets.widthHeightRatio,
+                        onChange: this.onInputChanged.bind(this, 'widthHeightRatio', parseFloat)
                     }),
-                    React.createElement('span', null, this.state.widthHeightRatio.toString())),
+                    React.createElement('span', null, sets.widthHeightRatio)),
                 React.createElement('div', { className: 'setting-single pad-top' },
                     React.createElement('h3', null, 'Spacing between Tiles'),
                     React.createElement('input', {
@@ -174,10 +164,10 @@ vivaldi.jdhooks.hookClass('settings_startpage_StartPage', cls => {
                         min: 0,
                         max: 100,
                         step: 1,
-                        value: this.state.spacing,
-                        onChange: this.onInputChanged.bind(this, 'spacing', 'value', parseInt)
+                        value: sets.spacing,
+                        onChange: this.onInputChanged.bind(this, 'spacing', parseInt)
                     }),
-                    React.createElement('span', null, this.state.spacing.toString() + 'px')),
+                    React.createElement('span', null, sets.spacing + 'px')),
                 React.createElement('div', { className: 'setting-single pad-top' },
                     React.createElement('h3', null, 'Margin on the Sides'),
                     React.createElement('input', {
@@ -185,27 +175,63 @@ vivaldi.jdhooks.hookClass('settings_startpage_StartPage', cls => {
                         min: 0,
                         max: 200,
                         step: 5,
-                        value: this.state.edgeMargin,
-                        onChange: this.onInputChanged.bind(this, 'edgeMargin', 'value', parseInt)
+                        value: sets.edgeMargin,
+                        onChange: this.onInputChanged.bind(this, 'edgeMargin', parseInt)
                     }),
-                    React.createElement('span', null, this.state.edgeMargin.toString() + 'px'))
-            );
+                    React.createElement('span', null, sets.edgeMargin + 'px'))
+            )
         }
+    }, { settings: ["SPEEDDIAL_SIZING"] })
+
+    const findChildParentName = "__spacing_parent"
+    function findChild(r, cb, recursive = false) {
+        if (r && r.props && r.props.children) {
+            if (!Array.isArray(r.props.children)) {
+                r.props.children[findChildParentName] = r
+                return cb(r.props.children)
+            }
+
+            for (let child of r.props.children) {
+                child[findChildParentName] = r
+                if (cb(child)) return child
+                if (recursive) {
+                    const inner = findChild(child, cb, recursive)
+                    if (inner) return inner
+                }
+            }
+        }
+        return null
+    }
+    function closest(r, cb) {
+        if (r) {
+            if (cb(r)) return r
+            if (r[findChildParentName]) return closest(r[findChildParentName], cb)
+        }
+        return null
     }
 
+    const isMaxColumns = c => c && c.props && c.props.className == "max-columns"
+    const isSettingGroup = c => c && c.props && c.props.className == "setting-group"
+
     class newCls extends cls {
-        constructor(...e) {
-            super(...e);
-        }
         render() {
-            let sp = super.render();
-            // TODO: more update-proof way to achieve this
-            let arr = sp.props.children[1].props.children[1].props.children;
-            arr.shift(); arr.shift();
-            let newOptions = React.createElement(TilesSection, this.props);
-            arr.unshift(newOptions);
-            return sp;
+            let sp = super.render()
+
+            const maxcol = findChild(sp, isMaxColumns, true)
+            if (maxcol) {
+                let group = closest(maxcol, isSettingGroup)
+                if (group && group.props && group.props.children) {
+                    for (const groupItemIdx in group.props.children) {
+                        if (findChild(group.props.children[groupItemIdx], isMaxColumns, true))
+                            group.props.children[groupItemIdx] = null
+                    }
+                    let newOptions = React.createElement(TilesSection, this.props)
+                    group.props.children.unshift(newOptions)
+                }
+            }
+
+            return sp
         }
     }
-    return newCls;
-});
+    return newCls
+})
