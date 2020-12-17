d3a934e01e173b2359b2dc302f564807198a1128
den_po <jdp@bk.ru>
2020-12-17 09:31:49 +0300

    remove archived mods

--- misc/archive/new-notes-at-the-top.js
+++
@@ -1,9 +0,0 @@
-//New notes at the top
-//Новые заметки в начале
-
-vivaldi.jdhooks.hookMember(window.vivaldi.notes, 'create', function(hookData, note, callback) {
-
-    if (undefined === note.index)
-        note.index = 0;
-
-});
